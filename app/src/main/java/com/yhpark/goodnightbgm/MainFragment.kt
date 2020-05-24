109cd89023bbfb46ec6871a2088965702158e360
yhpark <ppyh06>
2020-05-24 21:36:05 +0900

    add media volume control

--- app/src/main/java/com/yhpark/goodnightbgm/MainFragment.kt
+++ app/src/main/java/com/yhpark/goodnightbgm/MainFragment.kt
@@ -1,6 +1,10 @@
 package com.yhpark.goodnightbgm
 
+import android.annotation.SuppressLint
+import android.content.Context.AUDIO_SERVICE
 import android.content.SharedPreferences
+import android.media.AudioManager
+import android.media.AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -15,6 +19,7 @@ import java.text.SimpleDateFormat
 
 class MainFragment : Fragment() {
     lateinit var mediaPlayer: MediaPlayer
+    var preVolume: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,11 +29,14 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, null)
     }
 
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?
     ) {
         super.onViewCreated(view, savedInstanceState)
+        val audioManager = activity!!.getSystemService(AUDIO_SERVICE) as AudioManager
+        preVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         mediaPlayer = MediaPlayer.create(activity, getBgm())
 
@@ -36,35 +44,32 @@ class MainFragment : Fragment() {
             tvRemainTime.text = convertDuration(mediaPlayer.duration)
             progressBar.max = mediaPlayer.duration / 1000
 
-            val timer = object : CountDownTimer(20 * 1000/*mediaPlayer.duration.toLong()*/, 100) {
-                override fun onFinish() {
-                    activity!!.finishAffinity()
+            val timer =
+                object : CountDownTimer(1000 * 60 * 60 * 2/*mediaPlayer.duration.toLong()*/, 100) {
+                    override fun onFinish() {
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, preVolume, FLAG_REMOVE_SOUND_AND_VIBRATE)
+                        activity!!.finishAffinity()
 
-                    mediaPlayer.stop()
-                    mediaPlayer.release()
+                        mediaPlayer.stop()
+                        mediaPlayer.release()
+                    }
+
+                    override fun onTick(millisUntilFinished: Long) {
+                        tvCurrentTime.text =
+                            SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis())
+                        tvRemainTime.text =
+                            convertDuration(mediaPlayer.duration - mediaPlayer.currentPosition)
+
+                        progressBar.progress = mediaPlayer.currentPosition / 1000
+                    }
                 }
-
-                override fun onTick(millisUntilFinished: Long) {
-                    tvCurrentTime.text =
-                        SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis())
-                    tvRemainTime.text =
-                        convertDuration(mediaPlayer.duration - mediaPlayer.currentPosition)
-
-                    progressBar.progress = mediaPlayer.currentPosition / 1000
-                }
-            }
 
             imageButton.setOnClickListener {
                 if (mediaPlayer.isPlaying) {
-                    mediaPlayer.stop()
-                    mediaPlayer.seekTo(0)
-                    mediaPlayer.release()
-
-                    progressBar.progress = 0
-
-                    timer.cancel()
-                    imageButton.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+                    activity!!.finishAffinity()
                 } else {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 2, FLAG_REMOVE_SOUND_AND_VIBRATE)
+
                     timer.start()
                     mediaPlayer.start()
                     imageButton.setImageResource(R.drawable.ic_stop_black_24dp);
@@ -104,5 +109,11 @@ class MainFragment : Fragment() {
         val seconds = (duration % (1000 * 60 * 60) % (1000 * 60) / 1000).toString()
 
         return "$hours:$minutes:$seconds"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val audioManager = requireContext().getSystemService(AUDIO_SERVICE) as AudioManager
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, preVolume, FLAG_REMOVE_SOUND_AND_VIBRATE)
     }
 }
